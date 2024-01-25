fede3a48216fc2c8c3f00b10af7b283e174c49b9
Adil Khursheed <mughaladil571@gmail.com>
2024-01-26 02:37:07 +0500

    initial commit

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Computer:
+
+    def config(self):
+        print("config is i5 with 8gb ram")
+
+
+# Method 1 of calling methods of object
+# comp1 = Computer()
+# Computer.config(comp1)
+
+# Method 2
+com2 = Computer()
+com2.config()
